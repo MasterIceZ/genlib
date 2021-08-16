fdb73a6944374b1dfb61d4435e57f3b84db7bdb4
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2021-08-16 10:55:53 +0700

    example

--- example/gen.cpp
+++ example/gen.cpp
@@ -9,6 +9,6 @@ int main(){
 	printf("\n\n\n");
 	rand_connected_directed_weighted_graph(10, 20);
 	printf("\n\n\n");
-	rand_undirected_weighted_graph(10, 20);
+	rand_directed_weighted_graph(10, 20);
 	return 0;
 }
